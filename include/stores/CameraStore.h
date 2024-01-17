8a791bbec315a7e9d80b2edc984431eb60a11dae
swurl <swurl@swurl.xyz>
2024-01-16 20:25:51 -0500

    CameraServer
    Closes #14
    
    Signed-off-by: swurl <swurl@swurl.xyz>

--- include/stores/CameraStore.h
+++ include/stores/CameraStore.h
@@ -2,12 +2,35 @@
 #define CAMERASTORE_H
 
 #include <QObject>
+#include <QUrl>
+#include "networktables/NetworkTable.h"
+
+class Camera
+{
+private:
+    QUrl m_urls{};
+    QString m_name{};
+    QString m_source{};
+public:
+    Camera() = default;
+
+    static Camera fromTable(std::shared_ptr<nt::NetworkTable> table);
+
+    QList<QUrl> Urls;
+    QString Name;
+    QString Source;
+};
 
 class CameraStore : public QObject
 {
     Q_OBJECT
+private:
 public:
-    explicit CameraStore(QObject *parent = nullptr);
+    CameraStore();
+
+    static QList<Camera> Cameras;
+    static void filterCameras();
+    static Camera getCameraFromName(QString name);
 
 signals:
 };
