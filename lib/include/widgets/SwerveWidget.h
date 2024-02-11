87bafde9d67feffb16e8221353cace1e2ca992ff
swurl <swurl@swurl.xyz>
2024-02-11 01:09:03 -0500

    [widgets] Swerve
    Closes #36
    
    Implements a swerve widget that takes in module locations and module
    states from NT.
    
    Signed-off-by: swurl <swurl@swurl.xyz>

---
+++ lib/include/widgets/SwerveWidget.h
@@ -0,0 +1,13 @@
+#ifndef SWERVEWIDGET_H
+#define SWERVEWIDGET_H
+
+#include "widgets/BaseWidget.h"
+
+class SwerveWidget : public BaseWidget
+{
+    Q_OBJECT
+public:
+    SwerveWidget();
+};
+
+#endif // SWERVEWIDGET_H
